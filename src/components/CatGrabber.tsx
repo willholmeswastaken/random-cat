73e7efac5dedfe59d852721131b517cbd5e5b454
Will Holmes <willholmes97@icloud.com>
2022-12-29 23:50:07 +0000

    Added left and right buttons to cat grabber

--- src/components/CatGrabber.tsx
+++ src/components/CatGrabber.tsx
@@ -3,60 +3,61 @@ import type { Cat } from '../types';
 
 type State = {
     isLoading: boolean;
-    catUrl: string | null;
+    cat: string | null;
 };
 
 const CatGrabber = () => {
     const [catHistory, setCatHistory] = useState<Array<string>>([]);
-    const [catState, setCatState] = useState<State>({ isLoading: false, catUrl: null });
+    const [catState, setCatState] = useState<State>({ isLoading: false, cat: null });
 
-    const previous = (): void => {
+    const previousCat = (): void => {
         if (catHistory.length === 0) return;
         const prevCat = catHistory.pop();
-        setCatState({ ...catState, catUrl: prevCat! });
+        setCatState({ ...catState, cat: prevCat! });
         setCatHistory(catHistory.filter(x => x !== prevCat));
     }
 
-    const grabCatUrl = async (): Promise<void> => {
+    const fetchCat = async (): Promise<void> => {
         setCatState({ ...catState, isLoading: true });
         const res = await fetch("https://cataas.com/cat?json=true");
         const { url } = (await res.json()) as Cat;
         const catImageUrl = `https://cataas.com${url}`;
-        if (catState.catUrl) setCatHistory([...catHistory, catState.catUrl]);
-        setCatState({ ...catState, isLoading: false, catUrl: catImageUrl });
+        if (catState.cat) setCatHistory([...catHistory, catState.cat]);
+        setCatState({ ...catState, isLoading: false, cat: catImageUrl });
     };
 
     const prevDisabled = useMemo<boolean>(() => catHistory.length === 0, [catHistory]);
 
     useEffect(() => {
-        grabCatUrl();
+        fetchCat();
     }, [])
 
     return (
         <div className="flex flex-col gap-y-4">
-            <div className="w-80 h-80 border border-gray-300 rounded-md drop-shadow-sm">
-                {catState.catUrl && <img src={catState.catUrl} alt='cat' className='w-full h-full object-fill rounded-md animate-in fade-in duration-300 delay-150' />}
-            </div>
-
-            <div className="flex flex-row gap-x-2">
+            <div className="flex flex-row justify-center items-center">
                 <button
-                    type="button"
-                    className="rounded-md flex-1 disabled:bg-gray-400 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
-                    onClick={previous}
-                    disabled={prevDisabled}>
-                    Previous
+                    type='button'
+                    className='h-12 w-12 sm:h-14 sm:w-14 lg:h-16 lg:w-16 mr-2 rounded-full bg-indigo-500 hover:bg-indigo-600 text-white disabled:bg-gray-400 disabled:cursor-not-allowed'
+                    onClick={previousCat}
+                    disabled={prevDisabled || catState.isLoading}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 m-auto">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                    </svg>
                 </button>
+                <div className="lg:w-96 lg:h-96 md:w-80 md:h-80 w-52 h-48 border border-gray-300 rounded-md drop-shadow-sm">
+                    {catState.cat && <img
+                        src={catState.cat}
+                        alt='cat'
+                        className={`w-full h-full object-fill rounded-md animate-in fade-in duration-300 delay-150 ${catState.isLoading ? 'opacity-20' : 'opacity-100'}`} />}
+                </div>
                 <button
-                    type="button"
-                    className="rounded-md flex-1 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
-                    onClick={grabCatUrl}>
-                    {
-                        catState.isLoading && (<svg role="status" className="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
-                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
-                        </svg>)
-                    }
-                    Next
+                    type='button'
+                    className='h-12 w-12 sm:h-14 sm:w-14 lg:h-16 lg:w-16 ml-2 rounded-full bg-indigo-500 hover:bg-indigo-600 disabled:bg-gray-400 disabled:cursor-not-allowed text-white'
+                    onClick={fetchCat}
+                    disabled={catState.isLoading}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 m-auto">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+                    </svg>
                 </button>
             </div>
         </div>
