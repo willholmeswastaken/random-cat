703ce5fdb15cbea8cc6098b8a76cc23022281970
Will Holmes <willholmes97@icloud.com>
2022-12-29 19:25:46 +0000

    Initial version

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export interface Cat {
+  tags: any[];
+  createdAt: Date;
+  updatedAt: Date;
+  validated: boolean;
+  owner: string;
+  file: string;
+  mimetype: string;
+  size: number;
+  _id: string;
+  url: string;
+}
